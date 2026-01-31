d28d7def07959ef3fd3f735344ee7ac4908f79ec
cembatdal <cembatdal@gmail.com>
2026-01-31 14:29:28 +0300

    Refactor tile creation to use PackedScene instantiation
    Changed the tile creation in fill_bag_with_starter_tiles() to instantiate tiles from a preloaded PackedScene instead of using OkeyTile.new. This allows for scene-based tile management and ensures tiles are added as children to the node tree.

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,13 +3,17 @@ extends Node
 var bag:Array
 var hand:Array
 var hand_size:int = 14
+var okey_tile_scene: PackedScene = preload("res://scenes/okey_tile.tscn")
 
 func fill_bag_with_starter_tiles():
 	var starter_colors:Array = ["black", "red", "blue", "yellow"]
 	for tile_color in starter_colors:
 		for tile_number in range(1,14):
-			var new_tile:OkeyTile = OkeyTile.new(tile_color,tile_number)
+			var new_tile = okey_tile_scene.instantiate()
+			new_tile.okey_tile_color = tile_color
+			new_tile.okey_tile_number = tile_number
 			bag.append(new_tile)
+			add_child(new_tile)
 	
 func draw_starter_tiles_to_hand():
 	for i in range(hand_size):
